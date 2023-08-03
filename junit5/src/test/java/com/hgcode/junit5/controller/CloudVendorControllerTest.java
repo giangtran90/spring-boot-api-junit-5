9aada1b374b6790f8b3f2febd89215b881c04387
ADMIN <giang08t3@gmail.com>
2023-08-03 16:36:29 +0700

    Backend: controller : create CloudVendorControllerTest

---
+++ junit5/src/test/java/com/hgcode/junit5/controller/CloudVendorControllerTest.java
@@ -0,0 +1,36 @@
+package com.hgcode.junit5.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CloudVendorControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getCloudVendorDetails() {
+    }
+
+    @Test
+    void getAllCloudVendorDetails() {
+    }
+
+    @Test
+    void createCloudVendorDetails() {
+    }
+
+    @Test
+    void updateCloudVendorDetails() {
+    }
+
+    @Test
+    void deleteCloudVendorDetails() {
+    }
+}
