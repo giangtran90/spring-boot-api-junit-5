451fa40d20e23561efd60eabe351f3c3ac516a20
ADMIN <giang08t3@gmail.com>
2023-08-03 22:43:47 +0700

    Backend: test controller : implement testCreateCloudVendorDetails + testUpdateCloudVendorDetails

--- junit5/src/test/java/com/hgcode/junit5/controller/CloudVendorControllerTest.java
+++ junit5/src/test/java/com/hgcode/junit5/controller/CloudVendorControllerTest.java
@@ -1,5 +1,9 @@
 package com.hgcode.junit5.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.hgcode.junit5.model.CloudVendor;
 import com.hgcode.junit5.service.CloudVendorService;
 import org.junit.jupiter.api.AfterEach;
@@ -8,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -63,11 +68,29 @@ class CloudVendorControllerTest {
     }
 
     @Test
-    void createCloudVendorDetails() {
+    void testCreateCloudVendorDetails() throws Exception {
+        String requestJson = writeValueAsString(cloudVendorOne);
+
+        when(cloudVendorService.createCloudVendor(cloudVendorOne))
+                .thenReturn("Success");
+        this.mockMvc.perform(post("/cloudvendor/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void updateCloudVendorDetails() {
+    void testUpdateCloudVendorDetails() throws Exception {
+        String requestJson = writeValueAsString(cloudVendorOne);
+
+        when(cloudVendorService.updateCloudVendor(cloudVendorOne))
+                .thenReturn("Success");
+        this.mockMvc.perform(put("/cloudvendor/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -77,5 +100,13 @@ class CloudVendorControllerTest {
         this.mockMvc.perform(delete("/cloudvendor/1"))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    private String writeValueAsString(CloudVendor cloudVendor) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE,false);
+
+        ObjectWriter objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
+        return objectWriter.writeValueAsString(cloudVendor);
     }
 }
