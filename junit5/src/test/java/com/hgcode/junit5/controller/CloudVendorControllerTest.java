c87882e386a9c9b6c7fba51b395aca1d11cddc58
ADMIN <giang08t3@gmail.com>
2023-08-03 22:04:51 +0700

    Backend: test controller : implement testGetCloudVendorDetails

--- junit5/src/test/java/com/hgcode/junit5/controller/CloudVendorControllerTest.java
+++ junit5/src/test/java/com/hgcode/junit5/controller/CloudVendorControllerTest.java
@@ -1,13 +1,43 @@
 package com.hgcode.junit5.controller;
 
+import com.hgcode.junit5.model.CloudVendor;
+import com.hgcode.junit5.service.CloudVendorService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CloudVendorController.class)
 class CloudVendorControllerTest {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CloudVendorService cloudVendorService;
+
+    CloudVendor cloudVendorOne;
+    CloudVendor cloudVendorTwo;
+    List<CloudVendor> cloudVendorList = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
+        cloudVendorOne = new CloudVendor("1","Amazon","USA","11111");
+        cloudVendorTwo = new CloudVendor("2","Google","UK","22222");
+        cloudVendorList.add(cloudVendorOne);
+        cloudVendorList.add(cloudVendorTwo);
     }
 
     @AfterEach
@@ -15,11 +45,17 @@ class CloudVendorControllerTest {
     }
 
     @Test
-    void getCloudVendorDetails() {
+    void testGetCloudVendorDetails() throws Exception {
+        when(cloudVendorService.getCloudVendor("1"))
+                .thenReturn(cloudVendorOne);
+        this.mockMvc.perform(get("/cloudvendor/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
     void getAllCloudVendorDetails() {
+
     }
 
     @Test
